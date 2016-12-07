69b96bd12826e6852f34b4a5446d49b99a882516
bridgetlennon <blennon@sbacademy.org>
2016-12-07 15:32:06 -0800

    Create warmup.rb

---
+++ warmup.rb
@@ -0,0 +1,16 @@
+petnames = ["coco", "chloe", "sydney", "kozi", "frankie"]
+
+petnames.each do |names|
+ puts names
+end
+
+petnames = ["coco", "chloe", "sydney", "kozi", "frankie"]
+for names in petnames
+ puts names
+end
+
+=begin
+I like the 2nd loop better because it contains minimal
+symbols and I find it easier to read and understand off 
+the top of my head
+=end
